8e8f59157dfdcf9839d813fc42acc347b5e6e4a6
Ajay Sharma <aj@ajsharma.com>
2015-01-28 02:14:07 +0000

    Installs redis in /opt/ for easier finding later on

--- vagrant/install-redis.sh
+++ vagrant/install-redis.sh
@@ -3,8 +3,14 @@
 ####
 # This script can be used to install redis
 ####
+
+echo "Installing Redis"
 cd /tmp
 wget http://download.redis.io/releases/redis-2.8.19.tar.gz
-tar xzf redis-2.8.19.tar.gz
-cd redis-2.8.19
+cd /opt
+tar xzf /tmp/redis-2.8.19.tar.gz
+cd /opt/redis-2.8.19
 make
+
+echo "Redis installed"
+echo "Run Redis with `/opt/redis-2.8.19/src/redis-server"
